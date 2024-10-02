affac88aa3cadcf292a087657cbdad80b4decd3f
Luiz Vinicius <viniciusadr244@gmail.com>
2024-10-02 17:14:07 -0300

    feat: prometheus & grafana

--- src/main/java/luiz/sendmail/EmailSenderApplication.java
+++ src/main/java/luiz/sendmail/EmailSenderApplication.java
@@ -1,7 +1,11 @@
 package luiz.sendmail;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmailSenderApplication {
@@ -9,5 +13,8 @@ public class EmailSenderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmailSenderApplication.class, args);
 	}
-
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+		return registry -> registry.config().commonTags("application", applicationName);
+	}
 }
