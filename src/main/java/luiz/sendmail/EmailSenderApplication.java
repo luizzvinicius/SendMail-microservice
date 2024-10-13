74b41e488b12c3979ac4cb27a67a6524b14a8c29
Luiz Vinicius <viniciusadr244@gmail.com>
2024-10-13 12:28:54 -0300

    refactor: remove useless annotation

--- src/main/java/luiz/sendmail/EmailSenderApplication.java
+++ src/main/java/luiz/sendmail/EmailSenderApplication.java
@@ -5,11 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class EmailSenderApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmailSenderApplication.class, args);
