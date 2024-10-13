734f0c7334fc84d88d3c8618adb84e1e52db3792
Luiz Vinicius <viniciusadr244@gmail.com>
2024-10-13 11:09:26 -0300

    feat: eureka client

--- src/main/java/luiz/sendmail/controllers/SendMailController.java
+++ src/main/java/luiz/sendmail/controllers/SendMailController.java
@@ -5,10 +5,7 @@ import luiz.sendmail.dtos.SendMailDTO;
 import luiz.sendmail.service.SendMailService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
@@ -23,5 +20,10 @@ public class SendMailController {
     public ResponseEntity<Void> sendMail(@RequestBody @Valid SendMailDTO dto) {
         sendMailService.sendMail(dto.from(), dto.to(), dto.subject(), dto.content());
         return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<String> teste() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("erro ao acessar recurso tal");
     }
 }
